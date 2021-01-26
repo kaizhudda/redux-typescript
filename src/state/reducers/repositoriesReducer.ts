1b65df952e407cdeacf249b8d03ecf30be26e6c7
Kaiz Hudda <kaiz.hudda@gmail.com>
2021-01-26 21:27:25 +1300

    base setup: reducer

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,20 @@
+interface RepositoryState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepositoryState, action: any) => {
+  switch (action.type) {
+    case "search_repo":
+      return { loading: true, error: null, data: [] };
+    case "search_repo_success":
+      return { loading: false, error: null, data: action.payload };
+    case "search_repo_error":
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
