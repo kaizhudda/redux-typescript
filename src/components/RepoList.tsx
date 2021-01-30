efd820d2bfc195ffeef8d59e9f9b2bc93dda37a2
Kaiz Hudda <kaiz.hudda@gmail.com>
2021-01-30 16:22:24 +1300

    Add dispatch to react code using simple useActions hook

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
+
 const RepoList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepo } = useActions();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // dispatch(SearchRepo)
+    searchRepo(term);
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
